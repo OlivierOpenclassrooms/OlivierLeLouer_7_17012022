5680d69f570f78a56f64e2bac510dd14301dac5a
Olivier Le Louër <87853704+OlivierOpenclassrooms@users.noreply.github.com>
2022-04-09 18:43:09 -0400

    Update db.config.js

--- back/config/db.config.js
+++ back/config/db.config.js
@@ -1,13 +1,1 @@
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "Le12louer30!",
-  DB: "groupomania",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-};
+
